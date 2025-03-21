f3b3b00f08b802ad0143263c53af5345d831f322
Sumit Shinde <148746429+Sumit0769@users.noreply.github.com>
2025-03-21 21:54:52 +0530

    Update OCR_Flask.py

--- OCR_Flask.py
+++ OCR_Flask.py
@@ -11,8 +11,8 @@ UPLOAD_FOLDER = "static/uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # Path to Tesseract OCR
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
+#pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 def extract_details(image_path):
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
