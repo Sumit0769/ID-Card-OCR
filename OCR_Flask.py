3bb576c9a5793b17f6a29263226a55b39d9b248f
Sumit Shinde <148746429+Sumit0769@users.noreply.github.com>
2025-03-23 17:39:41 +0530

    Update OCR_Flask.py

--- OCR_Flask.py
+++ OCR_Flask.py
@@ -81,4 +81,4 @@ def index():
     return render_template("OCR HTML.html", extracted_data=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
